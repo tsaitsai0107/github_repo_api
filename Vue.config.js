00d18d543cf8442625f3e78e7dda694d69d49eb8
tsaitsai <vivian41521@yahoo.com.tw>
2021-02-24 19:41:58 +0800

    Initial commit

---
+++ Vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/github_repo_api/" : "/",
+};
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ username }}'s repositories</h1>
+  <div class="repositories">
+    <div class="repository" v-for="(repo, index) in repos" :key="index">
+      <div>
+        <div class="title">{{ repo.name }}</div>
+        <div class="text">
+          {{ repo.description }}
+        </div>
+      </div>
+      <a href="#">{{ repo.html_url }}</a>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      username: "tsaitsai0107",
+      repos: [],
+    };
+  },
+  mounted() {
+    const listElm = document.querySelector(".repositories");
+    listElm.addEventListener("scroll", () => {
+      if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+        this.loadMore();
+      }
+    });
+
+    this.loadMore();
+  },
+  methods: {
+    loadMore() {
+      axios
+        .get("https://api.github.com/users/tsaitsai0107/repos")
+        .then((response) => {
+          this.repos = this.repos.concat(response.data);
+        });
+    },
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro&family=Zilla+Slab&display=swap");
+body {
+  background-color: #fedfe1;
+  margin: 0;
+  /* box-sizing: border-box; */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1 {
+  text-align: center;
+  font-family: "Source Code Pro", Helvetica, Arial, sans-serif;
+}
+
+.repositories {
+  margin: auto;
+  width: 72vw;
+  background-color: #f8c3cd;
+  height: 500px;
+  overflow: scroll;
+  font-family: "Zilla Slab", Helvetica, Arial, sans-serif;
+}
+
+.repository {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+  padding: 40px;
+  border: 1px solid #777;
+  border-radius: 8px;
+}
+
+.title {
+  margin-bottom: 12px;
+  font-size: 24px;
+}
+
+a {
+  color: #777777;
 }
 </style>
